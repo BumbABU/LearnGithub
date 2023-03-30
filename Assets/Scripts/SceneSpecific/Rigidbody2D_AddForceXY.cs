261f0c6f84c5a3570e25a50a6189d8e37da572e2
THAIHOANG <ti.ngua4321@gmail.com>
2023-03-30 21:14:34 +0700

    asda

---
+++ Assets/Scripts/SceneSpecific/Rigidbody2D_AddForceXY.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Rigidbody2D_AddForceXY : MonoBehaviour
+{
+    public float forceX;
+    public float forceY;
+
+    private Rigidbody2D m_Rigidbody;
+
+    void Start()
+    {
+        m_Rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        m_Rigidbody.AddForce(new Vector2(forceX, forceY));
+        //m_Rigidbody.AddForceX(forceX);
+        //m_Rigidbody.AddForceY(forceY);
+    }
+}
---
+++ Assets/Scripts/SceneSpecific/Rigidbody2D_VelocityXY.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Rigidbody2D_VelocityXY : MonoBehaviour
+{
+    public float velocityX;
+    public float velocityY;
+
+    private Rigidbody2D m_Rigidbody;
+
+    void Start()
+    {
+        m_Rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        m_Rigidbody.velocity = new Vector2(velocityX, velocityY);
+        //m_Rigidbody.velocityX = velocityX;
+        //m_Rigidbody.velocityY = velocityY;
+    }
+}
